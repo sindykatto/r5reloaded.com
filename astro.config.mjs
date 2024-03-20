b1d779558a445208c0d805625cd18722ca036200
sindykatto <aria@justbecause.ph>
2024-03-21 04:01:11 +0800

    feat: added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 import { defineConfig } from 'astro/config';
 import nodejs from '@astrojs/node';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    build: {
-      inlineStylesheets: `always`,
-      format: 'file',
-    },
-    trailingSlash: "never",
-    site: 'https://r5reloaded.com',
-    adapter: nodejs({
-      mode: 'standalone'
-    }),
-    output: 'server',
-  });
+  build: {
+    inlineStylesheets: `always`,
+    format: 'file'
+  },
+  trailingSlash: "never",
+  site: 'https://r5reloaded.com',
+  adapter: nodejs({
+    mode: 'standalone'
+  }),
+  output: 'server',
+  integrations: [tailwind()]
+});
